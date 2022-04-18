91b847023238ad46d4830dcfc30496b322834ed1
Mikhail.Relkov <zzz@yyy.by>
2022-04-19 02:10:47 +0300

    Creat window and print button v.04

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ def application(): # Create windows and text and button
 
     # Create text
     main_text = QtWidgets.QLabel(window)
-    main_text.setText("!...Базовая надпись...!")  # create text
-    main_text.move(100, 100)  # move text the 100 pc
+    main_text.setText("        Напомнить про котлетки через:")  # create text
+    main_text.move(50, 20)  # move text the 100 pc
     main_text.adjustSize()  # embedding an object in the content
     main_text.setFixedWidth(200)
 
     # Create button
     btn = QtWidgets.QPushButton(window)  # create button
-    btn.move(70, 150)
+    btn.move(70, 40)
     btn.setText("Нажми меня")
     btn.setFixedWidth(200)
     btn.clicked.connect(add_label)  # объект.событие_объекта.метод(функция) Функцию не вызываем, только помещаем.
