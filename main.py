44473704b7c7dfbd5a390d6fadc407ef192194bc
Mikhail.Relkov <zzz@yyy.by>
2022-04-19 01:52:52 +0300

    Creat window and print button v.03

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 # Котлетный будильник.
-from PyQt5 import  QtWidgets
-from PyQt5.QtWidgets import  QApplication, QMainWindow
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-import  sys
+import sys
 
-def application ():
+
+def add_label():
+    print("Кнопка нажата")
+
+
+def application(): # Create windows and text and button
     app = QApplication(sys.argv)
-    window =QMainWindow()
+    window = QMainWindow()
 
     window.setWindowTitle("Cutlets alarm clock")
     window.setGeometry(300, 250, 350, 200)
 
-    window.show()
+    # Create text
+    main_text = QtWidgets.QLabel(window)
+    main_text.setText("!...Базовая надпись...!")  # create text
+    main_text.move(100, 100)  # move text the 100 pc
+    main_text.adjustSize()  # embedding an object in the content
+    main_text.setFixedWidth(200)
+
+    # Create button
+    btn = QtWidgets.QPushButton(window)  # create button
+    btn.move(70, 150)
+    btn.setText("Нажми меня")
+    btn.setFixedWidth(200)
+    btn.clicked.connect(add_label)  # объект.событие_объекта.метод(функция) Функцию не вызываем, только помещаем.
+
+    window.show()  # show windows
     sys.exit(app.exec_())
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     application()
