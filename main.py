eb50ce2a816fcb51289db2ba31fc994095abe272
Mikhail.Relkov <zzz@yyy.by>
2022-04-19 00:59:43 +0300

    Add window Qt5 v.02

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Котлетный будильник.
+from PyQt5 import  QtWidgets
+from PyQt5.QtWidgets import  QApplication, QMainWindow
+
+import  sys
+
+def application ():
+    app = QApplication(sys.argv)
+    window =QMainWindow()
+
+    window.setWindowTitle("Cutlets alarm clock")
+    window.setGeometry(300, 250, 350, 200)
+
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__=="__main__":
+    application()
