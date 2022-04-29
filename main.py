b57ff9ed0cc5804f0609e38d8d92388244d1815c
Mikhail.Relkov <zzz@yyy.by>
2022-04-30 00:55:10 +0300

    overwriting. add class v.07

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
 # Котлетный будильник.
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-import sys
-
-
-def add_label():
-    print("Кнопка нажата")
-
-
-def application(): # Create windows and text and button
-    app = QApplication(sys.argv)
-    window = QMainWindow()
-
-    window.setWindowTitle("Cutlets alarm clock")
-    window.setGeometry(300, 250, 350, 200)
-
-    # Create text
-    main_text = QtWidgets.QLabel(window)
-    main_text.setText("        Напомнить про котлетки через:")  # create text
-    main_text.move(50, 20)  # move text the 100 pc
-    main_text.adjustSize()  # embedding an object in the content
-    main_text.setFixedWidth(200)
-
-    # Create button
-    btn = QtWidgets.QPushButton(window)  # create button
-    btn.move(70, 40)
-    btn.setText("10 минут")
-    btn.setFixedWidth(200)
-    btn.clicked.connect(add_label)  # объект.событие_объекта.метод(функция) Функцию не вызываем, только помещаем.
-
-    window.show()  # show windows
-    sys.exit(app.exec_())
+# from PyQt5.QtWidgets import QApplication, QMainWindow
+#import sys
+from cutlet import Ui_MainWindow
 
 
 if __name__ == "__main__":
-    application()
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
